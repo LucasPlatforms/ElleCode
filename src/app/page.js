06da1f4433761e0867bf00b29efede375559428a
LucaL <jackhammer.dubstep@gmail.com>
2026-02-24 15:11:21 +0100

    rimosso about

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 //import Navbar from "../components/Navbar";
 //import Hero from "../components/Hero";
-import About from "../components/About";
+//import About from "../components/About";
 import Services from "../components/Services";
 import Projects from "../components/Projects";
 import FAQ from "../components/FAQ";
@@ -9,7 +9,6 @@ import Contact from "../components/Contact";
 export default function Home() {
   return (
     <main className="min-h-screen bg-zinc-950">
-      <About />
       <Services />
       <Projects />
       <FAQ />
