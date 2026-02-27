9b1dc311266ecd6a0653a8d003f67dea940fed3f
Luca <jackhammer.dubstep@gmail.com>
2026-02-27 19:27:38 +0100

    creazione sitemap e robots

---
+++ src/app/sitemap.js
@@ -0,0 +1,11 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://www.ellecode.it',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        // Aggiungi qui le altre rotte del tuo sito
+    ]
+}
